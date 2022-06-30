97b82fc3db86cdfba098d674ac5658a050ee2f30
Nonso-Marshal <anawanacu@gmail.com>
2022-06-30 11:34:53 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <studio.h>
 /**
- * main-Entry point
- * Return: 0 if successful
- */
+* main-Entry point
+* Return: 0 if successful
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+puts("\"Programming is like building a multilingual puzzle")
+return (0)
 }
 
