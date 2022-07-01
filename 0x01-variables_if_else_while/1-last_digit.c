fca0b4161ba52ad07950470b31a2c562a80f1e78
Nonso-Marshal <anawanacu@gmail.com>
2022-07-01 07:29:19 -0700

    if variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - prints "whether the number stored in tje variable is positive or negative"
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2
+
+	if(n>0){
+		printf("n is positive\n");
+	}
+	else if(n==o){
+		printf("n is zero\n");
+	}
+	else {
+		printf("n is negative\n");
+	return (0)
+	}
+}
