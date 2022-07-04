2f3f00052b8e97548860b4b6695c5168556ac371
Nonso-Marshal <anawanacu@gmail.com>
2022-07-04 10:46:14 -0700

    if variable

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single digit numbers
+ * Not allowed to use char
+ * Return: 0 ALways
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, x++)
+	{
+		putchar(x; x && x);
+	}
+	putchar if (A = x + x, single digit);
+	
+		putchar ('\n');
+		return (0);
+}
