541df4afd8af3575231968fad65cb43cf686bfb8
Nonso-Marshal <anawanacu@gmail.com>
2022-07-04 03:39:40 -0700

    if variable

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-printf '%.2f\n' $NUM
+#include <stdio.h>
+/**
+ * main - Program that print all the single digit of base 10
+ */
+
+int main(void)
+{
+	int base 10 = '1';
+	
+	while (base 10 <= '9');
+
+	putchar (base 10);
+		base 10 += 1;
+		putchar ('\n');
+		return (0);
+}
