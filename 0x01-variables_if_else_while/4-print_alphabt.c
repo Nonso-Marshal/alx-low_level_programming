ac9196e9c88f04bfe1434de41f4923a07846595f
Nonso-Marshal <anawanacu@gmail.com>
2022-07-04 02:07:22 -0700

    if variable

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program that prints alphabets in lowercase, except q and e
+ * You can use putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int lw = 'a';
+
+	while  (lw < = 'z');
+	if (!'q' && 'e');
+	putchar (lw);
+	lw += 1;
+	putchar ('\n');
+	return (0);
+}
