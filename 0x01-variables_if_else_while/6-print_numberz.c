2e8e5395a0a9646f3a1bacf4441112a56d35c0d4
Nonso-Marshal <anawanacu@gmail.com>
2022-07-04 09:26:22 -0700

    if variable

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,27 @@
  * Return: 0 Always
  */
 
-int main(void)
+int main()
 {
-	int num;
+	int x;
+	int k;
+	int y;
+	char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	char A[k];
+	while (x != 0)
 	{
-	 (0); num < 10; num++;
-		putchar((num % 10) + '0');
-	putchar ('\n');
+		y = x % 10;
+	
+		A[k++] = digits[y];
+
+		putchar(digits[y]);
+	
+	x = x / 10;
 	}
-		return (0);
+	printf("%i\n", y);
+	for (; k >= 0; --k)
+	{
+		putchar(A[k]);
+	}
+	return 0;
 }
