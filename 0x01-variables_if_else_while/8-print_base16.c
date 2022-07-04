2fa435c50cae21c5ec0e9c8dec1672780efa47a2
Nonso-Marshal <anawanacu@gmail.com>
2022-07-04 10:24:51 -0700

    if variable

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <studio>
+/**
+ * main - print all number of base 16
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char ch;
+	int x;
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
