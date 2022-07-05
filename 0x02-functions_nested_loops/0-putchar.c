324a6ee49872625171de3fadf9d8736d9ae91b9e
Nonso-Marshal <anawanacu@gmail.com>
2022-07-05 12:17:33 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,19 @@
 
 /** 
  * Main - Entry Point
- *
  * Return: 0 If successful
  */
 
 int main(void)
 {
-	_putchar( '_' );
-	_putchar( 'p' );
-	_putchar( 'u' );
-	_putchar( 't' );
-	_putchar( 'c' );
-	_putchar( 'h' );
-	_putchar( 'a' );
-	_putchar( 'r' );
-	_putchar( '\n' );
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
