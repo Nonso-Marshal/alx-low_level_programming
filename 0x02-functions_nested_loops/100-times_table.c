6ccfe6fdcd4c3c5999117d22f37dd10eaf2bed40
Nonso-Marshal <anawanacu@gmail.com>
2022-07-05 15:57:11 -0700

    nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,10 @@ void print_times_table(int n)
 
 	int a = 0, rep, b;
 
-	if (n < 0 || n >15)
+	if (n < 0 || n > 15)
 		return;
 
-	while(a <= n)
+	while (a <= n)
 	{
 		for (b = 0; b <= n; b++)
 		{
