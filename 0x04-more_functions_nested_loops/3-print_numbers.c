079b21c1bdf2cc0a789c4dd3f81557f18df96611
Nonso-Marshal <anawanacu@gmail.com>
2022-07-08 04:03:59 -0700

    more nested

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints the numbers, from 0 to 9, followed by a new line.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_puthar ('0' + n);
+		n++;
+
+	}
+	_putchar ('\n');
+
+}
