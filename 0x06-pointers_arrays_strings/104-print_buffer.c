733a1a61a6f2129ed043f5afdb31ee912040516d
Nonso-Marshal <anawanacu@gmail.com>
2022-07-14 09:56:09 -0700

    more pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,7 +13,9 @@
 void print_line(char *c, int s, int l)
 {
 	int j, k;
+
 	for (j = 0; j <= 9; j++)
+
 	{
 		if (j <= s)
 			printf("%02x", c[l * 10 + j]);
@@ -41,6 +43,7 @@ void print_line(char *c, int s, int l)
 void print_buffer(char *b, int size)
 {
 	int i;
+
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
